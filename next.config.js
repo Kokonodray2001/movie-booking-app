fe4ec1ef37963b72d4f8cbe78b2259705462455c
Kokonod Ray <64181896+Kokonodray2001@users.noreply.github.com>
2023-09-17 00:14:02 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GOOGLE_CLIENT_ID:
-      "540142837540-3k21l9qsn9g3vfld7e1j66nnt8tediso.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      "pk_test_51NqY6ISJL3iqb2tbhQUHx7rdbSZtxsN8VmYhx8lehBQMJ5ytHjOofwCWY5Ldyu5BDRIXA9frKIxLvELrDOLB2n2l00w5naysIb",
   },
 };
 module.exports = nextConfig;
